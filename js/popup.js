5336d1236381e33b4c66977393381ff0a2a63fc0
elenaterehovskaya <len-sh@yandex.ru>
2017-12-15 22:55:29 +0300

    Проверка итогового проекта «Нёрдс» - Работа над ошибками 2

--- js/popup.js
+++ js/popup.js
@@ -3,7 +3,6 @@ var link = document.querySelector(".button-writeus");
 var popup = document.querySelector(".modal-writeus");
 var close = popup.querySelector(".modal-close");
 
-var form = popup.querySelector(".feedback-form");
 var username = popup.querySelector("[name=username]");
 var email = popup.querySelector("[name=email]");
 
@@ -24,35 +23,12 @@ link.addEventListener("click", function (evt) {
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
-  username.classList.remove("feedback-username-error");
-  email.classList.remove("feedback-email-error");
-});
-
-form.addEventListener("submit", function (evt) {
-  if (!username.value && !email.value) {
-    evt.preventDefault();
-    username.classList.add("feedback-username-error");
-    email.classList.add("feedback-email-error");
-  } else {
-    localStorage.setItem("username", username.value);
-  }
-});
-
-form.addEventListener("submit", function (evt) {
-  if (username.value && !email.value) {
-    evt.preventDefault();
-    email.classList.add("feedback-email-error");
-  } else {
-    localStorage.setItem("username", username.value);
-  }
 });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     if (popup.classList.contains("modal-show")) {
       popup.classList.remove("modal-show");
-      username.classList.remove("feedback-username-error");
-      email.classList.remove("feedback-email-error");
     }
   }
 });
